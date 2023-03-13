caf36185ab9a598daa9aa364b3846a812ac443c6
Alex Iles <alexiles1995@gmail.com>
2023-03-13 14:03:10 +0000

    Add Icon Finder

---
+++ iconFinder.ps1
@@ -0,0 +1,15 @@
+﻿$json = Get-Content -Raw -Path C:\Users\alexi\Desktop\db.json | ConvertFrom-Json
+
+
+foreach($object_properties in $json.PsObject.Properties)
+{
+   foreach($value in $object_properties.value.PsObject.Properties) {
+     $jsonvalue = $value.Value.c
+      $Result = Invoke-WebRequest -URI "https://xivapi.com/action/$jsonvalue"| ConvertFrom-Json 
+      $value.Value | add-member Noteproperty iconLink $Result.Icon
+         Write-Output $value.Value  
+   }
+}
+
+$json | ConvertTo-Json -depth 100 | Out-File "C:\Users\alexi\Desktop\test.json"
+
