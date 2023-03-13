5c3efeb4dcc8e45f689f9a10cb176cb2b5a99713
Alex Iles <alexiles1995@gmail.com>
2023-03-13 14:49:56 +0000

    Update iconFinder.ps1
    update to use timestamps

--- iconFinder.ps1
+++ iconFinder.ps1
@@ -1,15 +1,24 @@
-﻿$json = Get-Content -Raw -Path C:\Users\alexi\Desktop\db.json | ConvertFrom-Json
+$json = Get-Content -Raw -Path C:\Users\alexi\Desktop\db.json | ConvertFrom-Json
 
 
 foreach($object_properties in $json.PsObject.Properties)
 {
    foreach($value in $object_properties.value.PsObject.Properties) {
-     $jsonvalue = $value.Value.c
+    if ($value.Value.deprecated -ne $null)
+    {
+        continue
+    }
+    if ($value.Value.c)
+        {
+            $jsonvalue = $value.Value.c
+        } else {
+            $jsonvalue = $value.Value.id
+        }
       $Result = Invoke-WebRequest -URI "https://xivapi.com/action/$jsonvalue"| ConvertFrom-Json 
-      $value.Value | add-member Noteproperty iconLink $Result.Icon
-         Write-Output $value.Value  
+      $value.Value | add-member Noteproperty iconLink $Result.Icon  
    }
 }
 
-$json | ConvertTo-Json -depth 100 | Out-File "C:\Users\alexi\Desktop\test.json"
+$timestamp = Get-Date -Format o | ForEach-Object { $_ -replace “:”, “.” }
 
+$json | ConvertTo-Json -depth 100 | Out-File "C:\Users\alexi\Desktop\$timestamp.json"
